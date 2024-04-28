a284d6cdaad78f8dc0937fcf0b38ad89fd98541e
Pranav Kokate <pranavkokate14@gmail.com>
2024-04-28 15:16:02 +0530

    pass gen with useRef, callback hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,130 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useCallback, useEffect, useRef } from "react";
+import "./App.css";
 
 function App() {
+  const [color, setColor] = useState("pink");
+
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  // useRef
+  const passwordRef = useRef(null);
+
+  // useCallback hook is used to memoize
+  const passGen = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRTSUVWXYZ";
+
+    if (numberAllowed) str += "1234567890";
+    if (charAllowed) str += "!@#$%^&*{}~`";
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed]);
+
+  useEffect(() => {
+    passGen();
+  }, [length, numberAllowed, charAllowed, passGen]);
+
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+      <h1 className="text-white text-center my-3">Password generator</h1>
+      <div className="flex shadow rounded-lg overflow-hidden mb-4">
+        <input
+          type="text"
+          value={password}
+          className="outline-none w-full py-1 px-3"
+          placeholder="Password"
+          readOnly
+          ref={passwordRef}
+        />
+        <button
+          onClick={copyPasswordToClipboard}
+          className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0"
         >
-          Learn React
-        </a>
-      </header>
+          copy
+        </button>
+      </div>
+      <div className="flex text-sm gap-x-2">
+        <div className="flex items-center gap-x-1">
+          <input
+            type="range"
+            min={6}
+            max={100}
+            value={length}
+            className="cursor-pointer"
+            onChange={(e) => {
+              setLength(e.target.value);
+            }}
+          />
+          <label>Length: {length}</label>
+        </div>
+        <div className="flex items-center gap-x-1">
+          <input
+            type="checkbox"
+            defaultChecked={numberAllowed}
+            id="numberInput"
+            onChange={() => {
+              setNumberAllowed((prev) => !prev);
+            }}
+          />
+          <label htmlFor="numberInput">Numbers</label>
+        </div>
+        <div className="flex items-center gap-x-1">
+          <input
+            type="checkbox"
+            defaultChecked={charAllowed}
+            id="characterInput"
+            onChange={() => {
+              setCharAllowed((prev) => !prev);
+            }}
+          />
+          <label htmlFor="characterInput">Characters</label>
+        </div>
+      </div>
     </div>
+
+    /*      <div
+      className="w-full h-screen duration-200 "
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 py-2 px-3 shadow-lg bg-white rounded-3xl">
+          <button
+            className="rounded-2xl w-20"
+            style={{ backgroundColor: "red" }}
+            onClick={() => setColor("red")}
+          >
+            Red
+          </button>
+          <button
+            className="rounded-2xl w-20"
+            style={{ backgroundColor: "green" }}
+            onClick={() => setColor("green")}
+          >
+            Green
+          </button>
+          <button
+            className="rounded-2xl w-20"
+            style={{ backgroundColor: "blue" }}
+            onClick={() => setColor("blue")}
+          >
+            Blue
+          </button>
+        </div>
+      </div>
+    </div>  */
   );
 }
 
